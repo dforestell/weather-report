d3ed631663f1cf6e734df6e204b8bb289cd3a27f
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-09-07 11:12:54 -0500

    add sun img

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './weather.scss';
+import SunImg from '../resources/images/sun.png'; 
 
 export default class Weather extends React.Component {
     constructor(props){
@@ -12,7 +13,7 @@ export default class Weather extends React.Component {
             <div className="weather-container">
                 <div className="weather-header">location</div>
                 <div className="inner-weather">
-                    <span className="weather-image">image</span>
+                    <span className="weather-image"><img src={SunImg}/></span>
                     <span className="temp">temp</span>
                 </div>
                 <div className="weather-footer"> weather status</div>
